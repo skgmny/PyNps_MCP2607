9b14cc547d50cb4019bed8596b4a59ab508f25f5
Choi Seok In <skgmny2488@gmail.com>
2026-08-25 20:03:51 +0900

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -122,7 +122,7 @@ st.title("국민연금 데이터 분석")
 company_name = st.text_input("회사명을 입력해 주세요", placeholder = "검색할 회사명 입력")
 
 
-if data and compant_name:
+if data and company_name:
     output = data.find_company(company_name=company_name)
 
 if len(output) > 0:
